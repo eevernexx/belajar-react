a7353da875d330b58aecee10c0067061674e9e2d
Aryasatya Muhammad Aqsel <aryasatyaaqsel@gmail.com>
2025-09-30 11:04:39 +0700

    menggunakan utility tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,29 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card bg-blue-200">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <div className='min-h-screen bg-gradient-to-b from-black to-pink-800 
+      flex flex-col items-center justify-center text-center'>
+        <div className='flex items-center justify-center space-x-8 mb-12'>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="w-24 h-24 animate-spin" alt="Vite logo" style={{animationDuration: "0.5s"}}/>
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="w-24 h-24 animate-spin" alt="React logo" style={{animationDuration: "0.5s"}} />
+          </a>
+        </div>
+        <h1 className='text-white text-6x1 font-bold mb-8 animate-spin' alt="React logo" style={{animationDuration: "0.5s"}}>Vite + React</h1>
+        <div className="text-sm space-y-6 mb-8">
+          <button onClick={() => setCount((count) => count + 1)} className='bg-white rounded-full px-6 py-3 font-semibold hover:bg-pink-300 transition animate-spin'>
+            count is {count}
+          </button>
+          <p className='text-neutral-50 animate-spin'>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="text-neutral-50 animate-spin">
+          Click on the Vite and React logos to learn more
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
